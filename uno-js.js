b2187168868cfa908a7dbe15eaeb2bf5f22e8a61
kaelwi <karoline.elisabeth.wild@seznam.cz>
2021-11-05 09:37:34 +0100

    start game - get data

--- uno-js.js
+++ uno-js.js
@@ -27,11 +27,39 @@ document.getElementById("playerNamesForm").addEventListener("submit", function(e
         player.push(document.getElementById("player2").value);
         player.push(document.getElementById("player3").value);
         player.push(document.getElementById("player4").value);
+        console.log(player.length)
         console.log(player);
         modal.hide();
+        startGame();
     } else {
         // Some action to prevent sending an empty name
     }
-    
-    
 })
+
+async function load(){
+    // hier starten wir gleich den request
+    // warten auf das promise (alternativ fetch, then notation)
+    let response = await fetch("http://nowaunoweb.azurewebsites.net/api/game/start",{
+        method: 'POST',
+        body: JSON.stringify(player),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
+    });
+
+    // dieser code wird erst ausgeführt wenn fetch fertig ist
+    if(response.ok){ // wenn http-status zwischen 200 und 299 liegt
+        // wir lesen den response body 
+        let result = await response.json(); // alternativ response.text wenn nicht json gewünscht ist
+        console.log(result);
+        // alert(JSON.stringify(result));
+    }else{
+        alert("HTTP-Error: " + response.status);
+    }
+}
+// hier rufen wir unsere asynchrone funktion auf
+// load();
+
+function startGame() {
+    load();
+}
